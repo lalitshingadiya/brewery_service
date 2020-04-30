a606ad7bc94d03841dc1e61625671911ce82ad2d
Lalitkumar Shingadiya <lalit.shingadiya@eclinicalworks.com>
2020-04-30 08:25:37 -0400

    Initial commit

---
+++ src/main/java/com/lds/brewery_service/BreweryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lds.brewery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreweryServiceApplication.class, args);
+    }
+
+}
